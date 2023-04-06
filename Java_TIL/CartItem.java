6df4ed0f3992640b165aa484e579d1318b39e246
seokmin <97sukmin@naver.com>
2023-04-06 14:23:37 +0900

    cartItem 정보 추가

--- Java_TIL/CartItem.java
+++ Java_TIL/CartItem.java
@@ -1,66 +1,52 @@
 
+
 public class CartItem {
 	// 장바구니에 담은 책 1권 정보
-	private String[] itemBook = new String[7];
+	// private String[] itemBook = new String[7];
+	
+	// 장바구니 담은 책 정
+	private Book itemBook;
 	// 장바구니에 담은 책 ISBN.
 	private String bookID;
 	// 장바구니에 담은 책 개수
 	private int quantity;
-	// 장바구니에 담은 책 가격(단가 * 가격)
+	// 장바구니에 담은 책 가격(단가 * 권수)
 	private int totalPrice;
 	
-	
+	// 입력값 없는 생성자
 	public CartItem() {
 		
 	}
-	// 매개변수 book: 장바구니 담은 책 1종류의 정보.
-	public CartItem(String[] book) {
-		this.itemBook = book;
-		this.bookID = book[0];
+	public CartItem(Book booklist) {
+		this.itemBook = booklist;
+		this.bookID = booklist.getBookId();
 		this.quantity = 1;
 		updateTotalPrice();
 	}
-	// 책정보 리턴
-	public String toStr() {
-		return "CartItem[bookID = " + bookID + ", quantity = " + quantity
-				+ ", totlaPrice = " + totalPrice + "]";
-	}
-	public String[] getItemBook() {
-		return itemBook;
-	}
-	public void setItemBook(String[] itemBook) {
-		this.itemBook = itemBook;
-	}
-	public String getBookID() {
-		return bookID;
-	}
+	public Book getItemBook() {return itemBook;}
+	public void setItemBook(Book itemBook) {this.itemBook = itemBook;}
+	
+	public void setTotalPrice(int totalPrice) {this.totalPrice = totalPrice;}
+	
+	public String getBookID() {return bookID;}
 	public void setBookID(String bookID) {
+		// 매개변수와 필드 이름이 bookID로 같다
+		// 필드: this.bookID
+		// 매개변수: bookID
 		this.bookID = bookID;
 		this.updateTotalPrice();
 	}
-	public int getQuantity() {
-		return quantity;
-	}
+	public int getQuantity() {return quantity;}
 	public void setQuantity(int quantity) {
+     	// 매개변수이름과 필드 이름 같음
+		// 필드: this.quantity
+		// 매개변수: quantity
 		this.quantity = quantity;
 		this.updateTotalPrice();
 	}
-	public int getTotalPrice() {
-		return totalPrice;
-	}
+	public int getTotalPrice() {return totalPrice;}
 	public void updateTotalPrice() {
-		totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
-	}
-	
-	
-	public static void main(String[] args) {
-		String[] cbook = new String[3]; 
-		cbook[0] = "ISBN1234"; cbook[1] = "JSP 30일 완성!!";cbook[2] = "330000";
-		
-		//CartItem 객체 생성
-		CartItem citem = new CartItem(cbook); // 인턴스변수 생성 // 생성자 호
-		System.out.println("citem.toStr() = " + citem.toStr());
-
+		totalPrice = this.itemBook.getUnitPrice() * this.quantity;
 	}
 
 }
